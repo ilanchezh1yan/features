dad80e886326b9f1445d7dc6874153b8f889cd8f
ILANCHEZHIYAN SAKTHIVEL <ilanchezhiyanaeropondy@gmail.com>
2024-12-06 19:30:10 +0530

    final firmware

--- oximeter/scr/AFR4990.c
+++ oximeter/scr/AFR4990.c
@@ -15,8 +15,9 @@
 #define CES_CMDIF_TYPE_DATA   0x02
 #define CES_CMDIF_PKT_STOP    0x0B
 
-#define BUFFER_SIZE  100
+#define BUFFER_SIZE  300
 #define WINDOW_SIZE  4
+#define NUMBER_LOCAL_PEAKS 50
 #define min(x,y) ((x) < (y) ? (x) : (y))
 
 #define D_RDY 20
@@ -82,47 +83,6 @@ static int32_t an_x[BUFFER_SIZE];
 static int32_t an_y[BUFFER_SIZE];
 static uint8_t brc;
 static uint16_t graphCount;
-#if 0
-void AFE4490_Init()
-{
-	AFE4490_Write(CONTROL0, CONTROL0_VAL);
-	AFE4490_Write(CONTROL0, SW_RST);
-	AFE4490_Write(TIAGAIN, TIAGAIN_VAL); // CF = 5pF, RF = 500kR
-	AFE4490_Write(TIA_AMB_GAIN, TIA_AMB_GAIN_VAL);
-	AFE4490_Write(LEDCNTRL, LEDCNTRL_VAL);
-	AFE4490_Write(CONTROL2, CONTROL2_VAL); // LED_RANGE=100mA, LED=50mA
-	AFE4490_Write(CONTROL1, CONTROL1_VAL); // Timers ON, average 3 samples
-	AFE4490_Write(PRPCOUNT, PRP);
-	AFE4490_Write(LED2STC, LED2STC_VAL);
-	AFE4490_Write(LED2ENDC, LED2ENDC_VAL);
-	AFE4490_Write(LED2LEDSTC, LED2LEDSTC_VAL);
-	AFE4490_Write(LED2LEDENDC, LED2LEDENDC_VAL);
-	AFE4490_Write(ALED2STC, ALED2STC_VAL);
-	AFE4490_Write(ALED2ENDC, ALED2ENDC_VAL);
-	AFE4490_Write(LED2CONVST, LED2CONVST_VAL);
-	AFE4490_Write(LED2CONVEND, LED2CONVEND_VAL);
-	AFE4490_Write(ALED2CONVST, ALED2CONVST_VAL);
-	AFE4490_Write(ALED2CONVEND, ALED2CONVEND_VAL);
-	AFE4490_Write(LED1STC, LED1STC_VAL);
-	AFE4490_Write(LED1ENDC, LED1ENDC_VAL);
-	AFE4490_Write(LED1LEDSTC, LED1LEDSTC_VAL);
-	AFE4490_Write(LED1LEDENDC, LED1LEDENDC_VAL);
-	AFE4490_Write(ALED1STC, ALED1STC_VAL);
-	AFE4490_Write(ALED1ENDC, ALED1ENDC_VAL);
-	AFE4490_Write(LED1CONVST, LED1CONVST_VAL);
-	AFE4490_Write(LED1CONVEND, LED1CONVEND_VAL);
-	AFE4490_Write(ALED1CONVST, ALED1CONVST_VAL);
-	AFE4490_Write(ALED1CONVEND, ALED1CONVEND_VAL);
-	AFE4490_Write(ADCRSTSTCT0, ADCRSTSTCT0_VAL);
-	AFE4490_Write(ADCRSTENDCT0, ADCRSTENDCT0_VAL);
-	AFE4490_Write(ADCRSTSTCT1, ADCRSTSTCT1_VAL);
-	AFE4490_Write(ADCRSTENDCT1, ADCRSTENDCT1_VAL);
-	AFE4490_Write(ADCRSTSTCT2, ADCRSTSTCT2_VAL);
-	AFE4490_Write(ADCRSTENDCT2, ADCRSTENDCT2_VAL);
-	AFE4490_Write(ADCRSTSTCT3, ADCRSTSTCT3_VAL);
-	AFE4490_Write(ADCRSTENDCT3, ADCRSTENDCT3_VAL);
-}
-#endif
 
 void AFE4490_Init()
 {
@@ -197,7 +157,7 @@ void GetSamples()
 
 		red_rx_buf[0] = red_rx_buf[0] & 0b00111111;
 		data.raw_red = red_rx_buf[0] << 16 | red_rx_buf[1] << 8 | red_rx_buf[2];
-		if(brc == 30) {
+		if(brc == 20) {
 			IR_LED_Graph[graphCount] = (uint16_t)(data.raw_IR >> 4);
 			RED_LED_Graph[graphCount] = (uint16_t)(data.raw_red >> 4);
 			graphCount++;
@@ -220,7 +180,7 @@ void GetSamples()
 		   Live_graph.CRC =  chksum8(&Live_graph.IR_lsb, 8);
 
 		BT_send((uint8_t *)&Live_graph);
-		send_data((uint8_t *)&Live_graph);
+		send_data((uint8_t *)&Live_graph, sizeof(Live_graph));
 
 		if(graphCount > BUFFER_SIZE - 1) {
 			estimate_spo2(IR_LED_Graph, BUFFER_SIZE, RED_LED_Graph, &(data.SpO2), &spo2_valid, &(data.Heart_Rate), &hr_valid);
@@ -241,11 +201,12 @@ void GetSamples()
 
 void estimate_spo2(uint16_t *pun_ir_buffer, int32_t n_ir_buffer_length, uint16_t *pun_red_buffer, uint8_t *pn_spo2, int8_t *pch_spo2_valid, uint8_t *pn_heart_rate, int8_t *pch_hr_valid)
 {
-  uint32_t un_ir_mean;
+
+  uint16_t ir_max, ir_min, ir_median;
   int32_t k, n_i_ratio_count;
   int32_t i, n_exact_ir_valley_locs_count, n_middle_idx;
   int32_t n_th1, n_npks;   
-  int32_t an_ir_valley_locs[15] ;
+  int32_t an_ir_valley_locs[NUMBER_LOCAL_PEAKS] = {0};
   int32_t n_peak_interval_sum;
   
   int32_t n_y_ac, n_x_ac;
@@ -256,79 +217,72 @@ void estimate_spo2(uint16_t *pun_ir_buffer, int32_t n_ir_buffer_length, uint16_t
   int32_t n_ratio_average; 
   int32_t n_nume, n_denom ;
 
-  // calculates DC mean and subtract DC from ir
-  un_ir_mean =0; 
-  for (k=0 ; k<n_ir_buffer_length ; k++ ) un_ir_mean += pun_ir_buffer[k] ;
-  un_ir_mean =un_ir_mean/n_ir_buffer_length ;
-    
-  // remove DC and invert signal so that we can use peak detector as valley detector
- for (k=0 ; k<n_ir_buffer_length ; k++ ) {  
-    an_x[k] = -1*(pun_ir_buffer[k] - un_ir_mean) ;
-  }
-    
-  //Moving Average
-  for(k=0; k< BUFFER_SIZE - WINDOW_SIZE ; k++){
-    //an_x[k]=( an_x[k]+an_x[k+1]+ an_x[k+2]+ an_x[k+3] + an_x[k+4] + an_x[k+5] + an_x[k+6] + an_x[k+7]) / (int)WINDOW_SIZE;
-	an_x[k]=( an_x[k]+an_x[k+1] + an_x[k+2]+ an_x[k+3]) / (int)WINDOW_SIZE;    
-  }
-  // calculate threshold  
-  n_th1=0; 
-  for ( k=0 ; k<BUFFER_SIZE ;k++){
-    n_th1 +=  an_x[k];
-  }
-  n_th1=  n_th1/ ( BUFFER_SIZE);
+  ir_max = find_max(pun_ir_buffer, BUFFER_SIZE);
 
-  for ( k=0 ; k<15;k++) an_ir_valley_locs[k]=0;
-  // since we flipped signal, we use peak detector as valley detector
-  find_peak( an_ir_valley_locs, &n_npks, an_x, BUFFER_SIZE, n_th1, 4, 15 );//peak_height, peak_distance, max_num_peaks 
+  for (k=0 ; k<n_ir_buffer_length ; k++ ) {  
+    an_x[k] = ir_max - pun_ir_buffer[k];
+  }
+
+  for(k = 0; k < BUFFER_SIZE - WINDOW_SIZE; k++) {
+    for(int j = 0; j < WINDOW_SIZE; j++) {
+        an_x[k] += an_x[k + j];
+    }
+    an_x[k] /= WINDOW_SIZE; 
+  }
+ 
+  n_th1=0;
+
+  ir_max = find_max((uint16_t *)an_x, BUFFER_SIZE);
+  ir_min = find_min((uint16_t *)an_x, BUFFER_SIZE);
+  n_th1 = (ir_max - ir_min) * 0.2 + ir_min;
+
+  find_peak( an_ir_valley_locs, &n_npks, an_x, BUFFER_SIZE, n_th1, 4, NUMBER_LOCAL_PEAKS );
+ 
   n_peak_interval_sum =0;
-  if (n_npks>=2){
+  if (n_npks > 2){
     for (k=1; k<n_npks; k++) n_peak_interval_sum += (an_ir_valley_locs[k] -an_ir_valley_locs[k -1] ) ;
-    n_peak_interval_sum =n_peak_interval_sum/(n_npks-1);
-    *pn_heart_rate =(uint8_t)((17` * 60)/ n_peak_interval_sum);
+    n_peak_interval_sum = n_peak_interval_sum/(n_npks-1);
+    *pn_heart_rate =(uint8_t)((25 * 60)/ n_peak_interval_sum);
     *pch_hr_valid  = 1;
   }
   else  { 
-    *pn_heart_rate = -1; // unable to calculate because # of peaks are too 
+    *pn_heart_rate = -1; 
     *pn_spo2 = -1;
     *pch_hr_valid  = 0;
+    *pch_spo2_valid = 0;
+     return;
   }
 
-  //  load raw value again for SPO2 calculation : RED(=y) and IR(=X)
   for (k=0 ; k<n_ir_buffer_length ; k++ )  {
       an_x[k] =  pun_ir_buffer[k] ; 
       an_y[k] =  pun_red_buffer[k] ; 
   }
 
-  // find precise min near an_ir_valley_locs
   n_exact_ir_valley_locs_count = n_npks; 
-  
-  //using exact_ir_valley_locs , find ir-red DC andir-red AC for SPO2 calibration an_ratio
-  //finding AC/DC maximum of raw
-
-  // find max between two valley locations 
-  // and use an_ratio betwen AC compoent of Ir & Red and DC compoent of Ir & Red for SPO2 
+   
   for (k=0; k< n_exact_ir_valley_locs_count-1; k++){
-    n_y_dc_max= -16777216 ; 
-    n_x_dc_max= -16777216; 
+    n_y_dc_max= 0; 
+    n_x_dc_max= 0; 
     if (an_ir_valley_locs[k+1]-an_ir_valley_locs[k] >3){
         for (i=an_ir_valley_locs[k]; i< an_ir_valley_locs[k+1]; i++){
           if (an_x[i]> n_x_dc_max) {n_x_dc_max =an_x[i]; n_x_dc_max_idx=i;}
           if (an_y[i]> n_y_dc_max) {n_y_dc_max =an_y[i]; n_y_dc_max_idx=i;}
       }
-      n_y_ac= (an_y[an_ir_valley_locs[k+1]] - an_y[an_ir_valley_locs[k] ] )*(n_y_dc_max_idx -an_ir_valley_locs[k]); //red
+      n_y_ac= (an_y[an_ir_valley_locs[k+1]] - an_y[an_ir_valley_locs[k] ] )*(n_y_dc_max_idx -an_ir_valley_locs[k]); 
       n_y_ac=  an_y[an_ir_valley_locs[k]] + n_y_ac/ (an_ir_valley_locs[k+1] - an_ir_valley_locs[k])  ; 
-      n_y_ac=  an_y[n_y_dc_max_idx] - n_y_ac;    // subracting linear DC compoenents from raw 
-      n_x_ac= (an_x[an_ir_valley_locs[k+1]] - an_x[an_ir_valley_locs[k] ] )*(n_x_dc_max_idx -an_ir_valley_locs[k]); // ir
+      n_y_ac=  an_y[n_y_dc_max_idx] - n_y_ac;   
+
+      n_x_ac= (an_x[an_ir_valley_locs[k+1]] - an_x[an_ir_valley_locs[k] ] )*(n_x_dc_max_idx -an_ir_valley_locs[k]); 
       n_x_ac=  an_x[an_ir_valley_locs[k]] + n_x_ac/ (an_ir_valley_locs[k+1] - an_ir_valley_locs[k]); 
-      n_x_ac=  an_x[n_y_dc_max_idx] - n_x_ac;      // subracting linear DC compoenents from raw 
-      n_nume=( n_y_ac *n_x_dc_max); //prepare X100 to preserve floating value
+      n_x_ac=  an_x[n_y_dc_max_idx] - n_x_ac; 
+  
+      n_nume=( n_y_ac *n_x_dc_max); 
       n_denom= ( n_x_ac *n_y_dc_max);
       an_ratio += (float)n_nume / (float)n_denom;
   }
 }
 an_ratio /= (float)k;
-*pn_spo2 = 120 - (34.3 * an_ratio);
+*pn_spo2 = 120 - (33.3 * an_ratio);
 if(*pn_spo2 > 100) *pn_spo2 = 100;
 }
 
@@ -342,15 +296,15 @@ void find_peak( int32_t *pn_locs, int32_t *n_npks,  int32_t  *pn_x, int32_t n_si
 
 void  find_peak_above( int32_t *pn_locs, int32_t *n_npks,  int32_t  *pn_x, int32_t n_size, int32_t n_min_height )
 {
-  int32_t i = 1, n_width;
+  uint16_t i = 5, n_width;
   *n_npks = 0;
 
-  while (i < n_size - 1) {
+  while (i < n_size - 5) {
     if (pn_x[i] > n_min_height && pn_x[i] > pn_x[i - 1]) {   // find left edge of potential peaks
       n_width = 1;
       while (i + n_width < n_size && pn_x[i] == pn_x[i + n_width]) // find flat peaks
         n_width++;
-      if (pn_x[i] > pn_x[i + n_width] && (*n_npks) < 15 ) {   // find right edge of peaks
+      if (pn_x[i] > pn_x[i - 5] &&  pn_x[i] > pn_x[i + 3] && (*n_npks) < NUMBER_LOCAL_PEAKS ) {   // find right edge of peaks
         pn_locs[(*n_npks)++] = i;
         // for flat peaks, peak location is left edge
         i += n_width + 1;
@@ -414,4 +368,25 @@ static uint8_t chksum8(const unsigned char *buff, size_t len)
         sum += *(buff++);
     return (uint8_t)sum;
 }
+
+static uint16_t find_max(uint16_t *data, uint16_t size)
+{
+	uint16_t max = 0;
+	while(size--){
+		if(data[size] > max) max = data[size];
+		
+	}
+	return max;
+}
+
+static uint16_t find_min(uint16_t *data, uint16_t size)
+{
+	uint16_t min = -1;
+	while(size--){
+		if(data[size] < min) min = data[size];
+		
+	}
+	return min;
+}
+
 
